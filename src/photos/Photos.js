a8fff991fff0d931eb43b5a7f23a3c483ae1ecda
mac503 <mac503@gmail.com>
2019-02-12 15:33:52 +0100

    class -> className

--- src/photos/Photos.js
+++ src/photos/Photos.js
@@ -3,7 +3,7 @@ import "./Photos.css";
 
 export default function Photos(props){
   return (
-    <div class="camera">
+    <div className="camera">
       <span>
         <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 18">
           <circle cx="12" cy="12" r="3.2"/>
