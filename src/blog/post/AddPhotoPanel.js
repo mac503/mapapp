f86c2ae9b6c35e56b1efa6fb88059d011dd35314
mac503 <mac503@gmail.com>
2019-02-14 18:42:17 +0100

    Show uploading photos indicator

--- src/blog/post/AddPhotoPanel.js
+++ src/blog/post/AddPhotoPanel.js
@@ -4,12 +4,9 @@ import Modal from "../../modal/Modal.js";
 import './AddPhotoPanel.css';
 
 export default function AddPhotoPanel(props){
-  //TODO find the null photos higher up the chain (or here?) by comparing them to the posts - find the ones which don't appear in any post
-  console.log(props.photos);
   const photos = props.photos.filter(x=>x.postId==null).map(photo => (
     <img key={photo.id} src={'/photos/'+photo.filename} data-id={photo.id} onClick={(e)=>props.addPhoto(e, props.postId)} />
   ));
-  console.log(photos);
   const content = (
     <div className='add-photos-panel'>
       <div className='photos-holder'>
--- src/photos/Photos.js
+++ src/photos/Photos.js
@@ -3,14 +3,15 @@ import "./Photos.css";
 
 export default function Photos(props){
   return (
-    <div className="camera">
+    <div className={'camera'+(props.isUploadingPhotos ? ' block' : '')}>
       <label>
+        {props.isUploadingPhotos ? <div className='is-uploading-indicator'>Uploading...</div> : null}
         <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" viewBox="0 0 24 18">
           <circle cx="12" cy="12" r="3.2"/>
           <path d="M9 2l-1.83 2h-3.17c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2v-12c0-1.1-.9-2-2-2h-3.17l-1.83-2h-6zm3 15c-2.76 0-5-2.24-5-5s2.24-5 5-5 5 2.24 5 5-2.24 5-5 5z"/>
             <path d="M0 0h24v24h-24z" fill="none"/>
         </svg>
-        <input type="file" multiple={true} accept="image/*" onChange={props.handleChange}/>
+        <input type="file" multiple={true} accept="image/*" disabled={props.isUploadingPhotos ? true : false} onChange={props.handleChange}/>
       </label>
     </div>
   );
