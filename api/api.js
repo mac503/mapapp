ac40d2c27059eaec1b7178ffb1fd38779af06444
mac503 <mac503@gmail.com>
2019-02-14 17:25:45 +0100

    Allow CORS to API, to get around dev proxy file upload issue.

--- api/api.js
+++ api/api.js
@@ -3,6 +3,14 @@ const app = express();
 const port = process.env.PORT || 5000;
 const routes = require('./routes');
 
+//TODO REMOVE CORS
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+//TODO REMOVE CORS
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
